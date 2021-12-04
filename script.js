434658ca1362151c92aa82313a70992939215e18
lopste <80974914+lopste@users.noreply.github.com>
2021-12-03 17:38:44 -0800

    add site files

---
+++ script.js
@@ -0,0 +1,17 @@
+const themes = {
+	"light": "--brand: #008cff; --text-color: black; --alt-text-color: white; --inner-border: none; --background: white; --side-color: #008cff;",
+	"dark": "--brand: #0077d9; --text-color: white; --alt-text-color: black; --inner-border: 2px white solid; --background: #1a1a1a; --side-color: #1a1a1a;"
+}
+var isDarkMode = 0;
+
+function toggleDarkMode() {
+	if (isDarkMode) {
+		isDarkMode = 0;
+		let styleObj = document.querySelector("html");
+		styleObj.style = themes.light;
+	} else {
+		isDarkMode = 1;
+		let styleObj = document.querySelector("html");
+		styleObj.style = themes.dark;
+	}
+};
