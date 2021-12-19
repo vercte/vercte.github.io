d66630df3fce42d9add719974cf0a0790a33e956
Vercte <80974914+lopste@users.noreply.github.com>
2021-12-19 14:12:04 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
+const stg = window.localStorage;
 const themes = {
 	"light": "--brand: #008cff; --text-color: black; --alt-text-color: white; --inner-border: none; --background: white; --side-color: #008cff;",
 	"dark": "--brand: #0077d9; --text-color: white; --alt-text-color: black; --inner-border: 2px #0077d9 solid; --background: #1a1a1a; --side-color: #1a1a1a;"
 }
-var isDarkMode = 0;
+if(stg.getItem("dm")) {
+	var isDarkMode = Number(stg.getItem("dm"));
+	let styleObj = document.querySelector("html");
+	if(isDarkMode == 1) {
+		styleObj.style = themes.dark;
+	} else {
+		styleObj.style = themes.light;
+	}
+} else {
+	var isDarkMode = 0;
+	stg.setItem("dm", 0);
+	
+	let styleObj = document.querySelector("html");
+	styleObj.style = themes.light;
+}
 
 function toggleDarkMode() {
 	if (isDarkMode) {
